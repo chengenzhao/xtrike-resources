071f68de278d5cb3a2876c19661e6a34daf02802
chengenzhao <chengen.zhao@mail.mcgill.ca>
2025-10-22 22:11:38 +0800

    releasing 0.1.1

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,4 +1,4 @@
 module com.whitewoodcity.xtrikeresources {
   exports com.whitewoodcity.xtrikeresources;
-  opens com.whitewoodcity.xtrikeresources;
+  opens com.whitewoodcity.xtrikeresources.clancy;
 }
