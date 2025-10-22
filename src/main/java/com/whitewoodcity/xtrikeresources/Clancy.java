34ffed1a1766acb0bdf79e1d374e7226ea676b3b
chengenzhao <chengen.zhao@mail.mcgill.ca>
2025-10-22 13:20:15 +0800

    1st commit

---
+++ src/main/java/com/whitewoodcity/xtrikeresources/Clancy.java
@@ -0,0 +1,4 @@
+package com.whitewoodcity.xtrikeresources;
+
+public class Clancy {
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,4 @@
+module com.whitewoodcity.xtrikeresources {
+  exports com.whitewoodcity.xtrikeresources;
+  opens com.whitewoodcity.xtrikeresources;
+}
