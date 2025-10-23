03a16ed09b75c59bb8aab047baaa18f6b4688690
chengenzhao <chengen.zhao@mail.mcgill.ca>
2025-10-23 11:47:58 +0800

    releasing 0.1.2

--- src/main/java/com/whitewoodcity/xtrikeresources/Clancy.java
+++ src/main/java/com/whitewoodcity/xtrikeresources/Clancy.java
@@ -1,4 +1,22 @@
 package com.whitewoodcity.xtrikeresources;
 
 public class Clancy {
+  public static final String HEAD = "head.jvg";
+  public static final String BODY_BACK = "body_back.jvg";
+  public static final String BODY_FRONT = "body_front.jvg";
+
+  public static final String LEFT_ARM = "left_arm.jvg";
+  public static final String LEFT_CALF = "left_calf.jvg";
+  public static final String LEFT_ELBOW = "left_elbow.jvg";
+  public static final String LEFT_KNEE = "left_knee.jvg";
+  public static final String LEFT_SHOE = "left_shoe.jvg";
+  public static final String LEFT_THIGH = "left_thigh.jvg";
+  public static final String LEFT_WRIST = "left_wrist.jvg";
+
+  public static final String RIGHT_ARM = "right_arm.jvg";
+  public static final String RIGHT_CALF = "right_calf.jvg";
+  public static final String RIGHT_KNEE = "right_knee.jvg";
+  public static final String RIGHT_SHOE = "right_shoe.jvg";
+  public static final String RIGHT_THIGH = "right_thigh.jvg";
+  public static final String RIGHT_WRIST = "right_wrist.jvg";
 }
