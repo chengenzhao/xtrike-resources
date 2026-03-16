374fdbf80cbcc4f329ec631e2dc25816b81e7bc7
chengenzhao <chengen.zhao@mail.mcgill.ca>
2026-03-16 15:27:03 +0800

    releasing 0.5.0

--- src/main/java/com/whitewoodcity/xtrikeresources/clancy/Clancy.java
+++ src/main/java/com/whitewoodcity/xtrikeresources/clancy/Clancy.java
@@ -6,6 +6,14 @@ import static com.whitewoodcity.xtrikeresources.clancy.Action.*;
 import static com.whitewoodcity.xtrikeresources.clancy.Component.*;
 
 public class Clancy {
+  /**
+   * Replaced by CASUAL_COMPONENTS
+   */
+  @Deprecated(since = ".5", forRemoval = true)
   public static final List<Component> ORDERED_COMPONENTS = List.of(RIGHT_ARM, RIGHT_WRIST, RIGHT_CALF, RIGHT_SHOE, RIGHT_THIGH, RIGHT_KNEE, LEFT_CALF, LEFT_SHOE, LEFT_THIGH, LEFT_KNEE, BODY_BACK, HEAD, BODY_FRONT, LEFT_ARM, LEFT_WRIST, LEFT_ELBOW);
+
+  public static final List<Component> CASUAL_COMPONENTS = List.of(RIGHT_ARM, RIGHT_WRIST, RIGHT_CALF, RIGHT_SHOE, RIGHT_THIGH, RIGHT_KNEE, LEFT_CALF, LEFT_SHOE, LEFT_THIGH, LEFT_KNEE, BODY_BACK, HEAD, BODY_FRONT, LEFT_ARM, LEFT_WRIST, LEFT_ELBOW);
   public static final List<Action> ACTIONS = List.of(IDLE, WALK, RUN, JUMP);
+  public static final List<Component> BATTLE_COMPONENTS = List.of(RIGHT_ARM, RIGHT_WRIST, RIGHT_CALF, RIGHT_SHOE, RIGHT_THIGH, RIGHT_KNEE, LEFT_CALF, LEFT_SHOE, LEFT_THIGH, LEFT_KNEE, BODY_BACK, HEAD, BODY_FRONT, SCARL, LEFT_ARM, LEFT_WRIST, LEFT_ELBOW);
+
 }
