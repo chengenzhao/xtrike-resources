c15a342530584f460f8b98a69435d3fab0047fd2
chengenzhao <chengen.zhao@mail.mcgill.ca>
2026-03-25 14:39:56 +0800

    bump resources to 0.7

--- src/main/java/com/whitewoodcity/xtrikeresources/clancy/Clancy.java
+++ src/main/java/com/whitewoodcity/xtrikeresources/clancy/Clancy.java
@@ -18,13 +18,13 @@ public class Clancy {
   public static final List<Action> ACTIONS = List.of(IDLE, WALK, RUN, JUMP);
 
   public static final List<JVGComponent> CASUAL_JVG_COMPONENTS = List.of(RIGHT_ARM, RIGHT_WRIST, RIGHT_CALF, RIGHT_SHOE, RIGHT_THIGH, RIGHT_KNEE, LEFT_CALF, LEFT_SHOE, LEFT_THIGH, LEFT_KNEE, BODY_BACK, HEAD, BODY_FRONT, LEFT_ARM, LEFT_WRIST, LEFT_ELBOW);
-  public static final List<Action> CASUAL_ACTIONS = List.of(IDLE, WALK, RUN, JUMP);
+  public static final List<Action> CASUAL_ACTIONS              = List.of(IDLE, WALK, RUN, JUMP);
 
   public static final List<JVGComponent> BATTLE_JVG_COMPONENTS = List.of(RIGHT_ARM, RIGHT_WRIST, RIGHT_CALF, RIGHT_SHOE, RIGHT_THIGH, RIGHT_KNEE, LEFT_CALF, LEFT_SHOE, LEFT_THIGH, LEFT_KNEE, BODY_BACK, HEAD, BODY_FRONT, SCARL, LEFT_ARM, LEFT_WRIST, LEFT_ELBOW, RIGHT_PALM);
-  public static final List<JVGComponent> BATTLE_UPPER_BODY = List.of(RIGHT_ARM, RIGHT_WRIST, BODY_BACK, HEAD, BODY_FRONT, SCARL, LEFT_ARM, LEFT_WRIST, LEFT_ELBOW, RIGHT_PALM);
-  public static final List<Action> BATTLE_UPPER_ACTIONS = List.of(ALERT, SHOOT, FIRE);
-  public static final List<JVGComponent> BATTLE_LOWER_BODY = List.of(RIGHT_CALF, RIGHT_SHOE, RIGHT_THIGH, RIGHT_KNEE, LEFT_CALF, LEFT_SHOE, LEFT_THIGH, LEFT_KNEE);
-  public static final List<Action> BATTLE_LOWER_ACTIONS = List.of(IDLE, WALK, RUN, JUMP);
-  public static final List<JVGComponent> FLAME_JVG_COMPONENTS = List.of(FLAME0, FLAME1, FLAME2, FLAME3, FLAME4, FLAME5);
-  public static final List<Action> FLAME_ACTIONS        = List.of(FLAME);
+  public static final List<JVGComponent> BATTLE_UPPER_BODY     = List.of(RIGHT_ARM, RIGHT_WRIST, BODY_BACK, HEAD, BODY_FRONT, SCARL, LEFT_ARM, LEFT_WRIST, LEFT_ELBOW, RIGHT_PALM);
+  public static final List<Action> BATTLE_UPPER_ACTIONS        = List.of(ALERT, SHOOT, FIRE);
+  public static final List<JVGComponent> BATTLE_LOWER_BODY     = List.of(RIGHT_CALF, RIGHT_SHOE, RIGHT_THIGH, RIGHT_KNEE, LEFT_CALF, LEFT_SHOE, LEFT_THIGH, LEFT_KNEE);
+  public static final List<Action> BATTLE_LOWER_ACTIONS        = List.of(IDLE, WALK, RUN, JUMP);
+  public static final List<JVGComponent> FLAME_JVG_COMPONENTS  = List.of(FLAME0, FLAME1, FLAME2, FLAME3, FLAME4, FLAME5);
+  public static final List<Action> FLAME_ACTIONS               = List.of(FLAME);
 }
