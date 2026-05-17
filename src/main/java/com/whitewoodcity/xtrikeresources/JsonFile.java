1cf349a4b2a32fd40e1dbcc69f8e1acbcc294c7a
chengenzhao <chengen.zhao@mail.mcgill.ca>
2026-05-17 18:07:29 +0800

    releasing 0.9.1

--- src/main/java/com/whitewoodcity/xtrikeresources/JsonFile.java
+++ src/main/java/com/whitewoodcity/xtrikeresources/JsonFile.java
@@ -4,6 +4,7 @@ public interface JsonFile {
 
   String getFileName();
 
+  //make sure the jvg files are in the same folder of class file
   default String getJsonString(){
     try (var inputStream = getClass().getResourceAsStream(getFileName())){
       return new String(inputStream.readAllBytes());
--- src/main/java/com/whitewoodcity/xtrikeresources/clancy/Clancy.java
+++ src/main/java/com/whitewoodcity/xtrikeresources/clancy/Clancy.java
@@ -6,17 +6,6 @@ import static com.whitewoodcity.xtrikeresources.clancy.Action.*;
 import static com.whitewoodcity.xtrikeresources.clancy.JVGComponent.*;
 
 public class Clancy {
-  /**
-   * Replaced by CASUAL_COMPONENTS
-   */
-  @Deprecated(since = "0.5", forRemoval = true)
-  public static final List<JVGComponent> ORDERED_JVG_COMPONENTS = List.of(RIGHT_ARM, RIGHT_WRIST, RIGHT_CALF, RIGHT_SHOE, RIGHT_THIGH, RIGHT_KNEE, LEFT_CALF, LEFT_SHOE, LEFT_THIGH, LEFT_KNEE, BODY_BACK, HEAD, BODY_FRONT, LEFT_ARM, LEFT_WRIST, LEFT_ELBOW);
-  /**
-   * Replaced by CASUAL_ACTIONS
-   */
-  @Deprecated(since = "0.5.1", forRemoval = true)
-  public static final List<Action> ACTIONS = List.of(IDLE, WALK, RUN, JUMP);
-
   public static final List<JVGComponent> CASUAL_JVG_COMPONENTS = List.of(RIGHT_ARM, RIGHT_WRIST, RIGHT_CALF, RIGHT_SHOE, RIGHT_THIGH, RIGHT_KNEE, LEFT_CALF, LEFT_SHOE, LEFT_THIGH, LEFT_KNEE, BODY_BACK, HEAD, BODY_FRONT, LEFT_ARM, LEFT_WRIST, LEFT_ELBOW);
   public static final List<Action> CASUAL_ACTIONS              = List.of(IDLE, WALK, RUN, JUMP);
 
