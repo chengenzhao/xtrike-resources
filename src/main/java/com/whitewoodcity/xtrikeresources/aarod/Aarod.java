b13e45ef4fd19048d7a3a980ef9d8fabd751c9ea
chengenzhao <chengen.zhao@mail.mcgill.ca>
2026-06-09 13:23:35 +0800

    feat: adjust components order of Aarod

--- src/main/java/com/whitewoodcity/xtrikeresources/aarod/Aarod.java
+++ src/main/java/com/whitewoodcity/xtrikeresources/aarod/Aarod.java
@@ -6,6 +6,6 @@ import static com.whitewoodcity.xtrikeresources.aarod.Action.IDLE;
 import static com.whitewoodcity.xtrikeresources.aarod.JVGComponent.*;
 
 public class Aarod {
-  public static final List<JVGComponent> JVG_COMPONENTS = List.of(RIGHT_THIGH,RIGHT_CALF,RIGHT_FOOT, LEFT_THIGH, LEFT_CALF, LEFT_FOOT, LEFT_ARM, LEFT_HAND,RIGHT_ARM, RIGHT_HAND,RIGHT_SHOULDER,BODY, LEFT_SHOULDER, HEAD);
-  public static final List<Action> ACTIONS              = List.of(IDLE);
+  public static final List<JVGComponent> JVG_COMPONENTS = List.of(RIGHT_ARM, RIGHT_HAND, RIGHT_THIGH, RIGHT_CALF, RIGHT_FOOT, LEFT_THIGH, LEFT_CALF, LEFT_FOOT, LEFT_ARM, LEFT_HAND, RIGHT_SHOULDER, BODY, LEFT_SHOULDER, HEAD);
+  public static final List<Action> ACTIONS = List.of(IDLE);
 }
