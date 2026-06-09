c55661bfec63a976c3bf6f127378de68c46143f7
chengenzhao <chengen.zhao@mail.mcgill.ca>
2026-06-09 22:43:40 +0800

    releasing 1.0.1

--- src/main/java/com/whitewoodcity/xtrikeresources/aarod/Aarod.java
+++ src/main/java/com/whitewoodcity/xtrikeresources/aarod/Aarod.java
@@ -1,11 +1,14 @@
 package com.whitewoodcity.xtrikeresources.aarod;
 
+import com.whitewoodcity.xtrikeresources.JVGComponent;
+import com.whitewoodcity.xtrikeresources.JsonFile;
+
 import java.util.List;
 
 import static com.whitewoodcity.xtrikeresources.aarod.Action.IDLE;
 import static com.whitewoodcity.xtrikeresources.aarod.Components.*;
 
 public class Aarod {
-  public static final List<Components> JVG_COMPONENTS = List.of(RIGHT_ARM, RIGHT_HAND, RIGHT_THIGH, RIGHT_CALF, RIGHT_FOOT, LEFT_THIGH, LEFT_CALF, LEFT_FOOT, LEFT_ARM, LEFT_HAND, RIGHT_SHOULDER, BODY, LEFT_SHOULDER, HEAD);
-  public static final List<Action> ACTIONS = List.of(IDLE);
+  public static final List<JVGComponent> JVG_COMPONENTS = List.of(RIGHT_ARM, RIGHT_HAND, RIGHT_THIGH, RIGHT_CALF, RIGHT_FOOT, LEFT_THIGH, LEFT_CALF, LEFT_FOOT, LEFT_ARM, LEFT_HAND, RIGHT_SHOULDER, BODY, LEFT_SHOULDER, HEAD);
+  public static final List<JsonFile> ACTIONS = List.of(IDLE);
 }
--- src/main/java/com/whitewoodcity/xtrikeresources/clancy/Clancy.java
+++ src/main/java/com/whitewoodcity/xtrikeresources/clancy/Clancy.java
@@ -1,19 +1,22 @@
 package com.whitewoodcity.xtrikeresources.clancy;
 
+import com.whitewoodcity.xtrikeresources.JVGComponent;
+import com.whitewoodcity.xtrikeresources.JsonFile;
+
 import java.util.List;
 
 import static com.whitewoodcity.xtrikeresources.clancy.Action.*;
 import static com.whitewoodcity.xtrikeresources.clancy.Components.*;
 
 public class Clancy {
-  public static final List<Components> CASUAL_JVG_COMPONENTS = List.of(RIGHT_ARM, RIGHT_WRIST, RIGHT_CALF, RIGHT_SHOE, RIGHT_THIGH, RIGHT_KNEE, LEFT_CALF, LEFT_SHOE, LEFT_THIGH, LEFT_KNEE, BODY_BACK, HEAD, BODY_FRONT, LEFT_ARM, LEFT_WRIST, LEFT_ELBOW);
-  public static final List<Action> CASUAL_ACTIONS              = List.of(IDLE, WALK, RUN, JUMP);
+  public static final List<JVGComponent> CASUAL_JVG_COMPONENTS = List.of(RIGHT_ARM, RIGHT_WRIST, RIGHT_CALF, RIGHT_SHOE, RIGHT_THIGH, RIGHT_KNEE, LEFT_CALF, LEFT_SHOE, LEFT_THIGH, LEFT_KNEE, BODY_BACK, HEAD, BODY_FRONT, LEFT_ARM, LEFT_WRIST, LEFT_ELBOW);
+  public static final List<JsonFile> CASUAL_ACTIONS              = List.of(IDLE, WALK, RUN, JUMP);
 
-  public static final List<Components> BATTLE_JVG_COMPONENTS = List.of(RIGHT_ARM, RIGHT_WRIST, RIGHT_CALF, RIGHT_SHOE, RIGHT_THIGH, RIGHT_KNEE, LEFT_CALF, LEFT_SHOE, LEFT_THIGH, LEFT_KNEE, BODY_BACK, HEAD, BODY_FRONT, SCARL, LEFT_ARM, LEFT_WRIST, LEFT_ELBOW, RIGHT_PALM);
-  public static final List<Components> BATTLE_UPPER_BODY     = List.of(RIGHT_ARM, RIGHT_WRIST, BODY_BACK, HEAD, BODY_FRONT, SCARL, LEFT_ARM, LEFT_WRIST, LEFT_ELBOW, RIGHT_PALM);
-  public static final List<Action> BATTLE_UPPER_ACTIONS        = List.of(ALERT, SHOOT, FIRE);
-  public static final List<Components> BATTLE_LOWER_BODY     = List.of(RIGHT_CALF, RIGHT_SHOE, RIGHT_THIGH, RIGHT_KNEE, LEFT_CALF, LEFT_SHOE, LEFT_THIGH, LEFT_KNEE);
-  public static final List<Action> BATTLE_LOWER_ACTIONS        = List.of(IDLE, WALK, RUN, JUMP);
-  public static final List<Components> FLAME_JVG_COMPONENTS  = List.of(FLAME0, FLAME1, FLAME2, FLAME3, FLAME4, FLAME5);
-  public static final List<Action> FLAME_ACTIONS               = List.of(FLAME);
+  public static final List<JVGComponent> BATTLE_JVG_COMPONENTS = List.of(RIGHT_ARM, RIGHT_WRIST, RIGHT_CALF, RIGHT_SHOE, RIGHT_THIGH, RIGHT_KNEE, LEFT_CALF, LEFT_SHOE, LEFT_THIGH, LEFT_KNEE, BODY_BACK, HEAD, BODY_FRONT, SCARL, LEFT_ARM, LEFT_WRIST, LEFT_ELBOW, RIGHT_PALM);
+  public static final List<JVGComponent> BATTLE_UPPER_BODY     = List.of(RIGHT_ARM, RIGHT_WRIST, BODY_BACK, HEAD, BODY_FRONT, SCARL, LEFT_ARM, LEFT_WRIST, LEFT_ELBOW, RIGHT_PALM);
+  public static final List<JsonFile> BATTLE_UPPER_ACTIONS        = List.of(ALERT, SHOOT, FIRE);
+  public static final List<JVGComponent> BATTLE_LOWER_BODY     = List.of(RIGHT_CALF, RIGHT_SHOE, RIGHT_THIGH, RIGHT_KNEE, LEFT_CALF, LEFT_SHOE, LEFT_THIGH, LEFT_KNEE);
+  public static final List<JsonFile> BATTLE_LOWER_ACTIONS        = List.of(IDLE, WALK, RUN, JUMP);
+  public static final List<JVGComponent> FLAME_JVG_COMPONENTS  = List.of(FLAME0, FLAME1, FLAME2, FLAME3, FLAME4, FLAME5);
+  public static final List<JsonFile> FLAME_ACTIONS               = List.of(FLAME);
 }
