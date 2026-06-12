ab7f0d168d843604001cb4880c04255079bcdee3
chengenzhao <chengen.zhao@mail.mcgill.ca>
2026-06-12 11:36:25 +0800

    feat: bones' blade

---
+++ src/main/java/com/whitewoodcity/xtrikeresources/monsters/bones/Components.java
@@ -0,0 +1,25 @@
+package com.whitewoodcity.xtrikeresources.monsters.bones;
+
+import com.whitewoodcity.xtrikeresources.JVGComponent;
+
+public enum Components implements JVGComponent {
+  BLADE("blade.jvg",1),
+  ;
+
+  final String jvg;
+  final int rotateNum;
+
+  Components(String jvg, int rotateNum) {
+    this.jvg = jvg;
+    this.rotateNum = rotateNum;
+  }
+
+  public int rotateNum() {
+    return rotateNum;
+  }
+
+  @Override
+  public String getFileName() {
+    return jvg;
+  }
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,4 +4,5 @@ open module com.whitewoodcity.xtrikeresources {
   exports com.whitewoodcity.xtrikeresources.yang;
   exports com.whitewoodcity.xtrikeresources.aarod;
   exports com.whitewoodcity.xtrikeresources.landscape;
+  exports com.whitewoodcity.xtrikeresources.monsters.bones;
 }
