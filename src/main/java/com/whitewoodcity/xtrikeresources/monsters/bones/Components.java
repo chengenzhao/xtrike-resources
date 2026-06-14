0a6424f15a4e767371f30e6803a78aa26ceb1fc6
chengenzhao <chengen.zhao@mail.mcgill.ca>
2026-06-14 18:22:36 +0800

    releasing 1.0.12
    bones idle status

--- src/main/java/com/whitewoodcity/xtrikeresources/monsters/bones/Components.java
+++ src/main/java/com/whitewoodcity/xtrikeresources/monsters/bones/Components.java
@@ -3,14 +3,14 @@ package com.whitewoodcity.xtrikeresources.monsters.bones;
 import com.whitewoodcity.xtrikeresources.JVGComponent;
 
 public enum Components implements JVGComponent {
-  BLADE("blade.jvg",1),
+  BLADE("blade.jvg",2),
   HEAD("head.jvg",1),
   RIGHT_ARM("right_arm.jvg",1),
   LEFT_SHOULDER("left_shoulder.jvg",1),
   LEFT_ARM("left_arm.jvg",1),
-  LEFT_HAND("left_hand.jvg",1),
-  LEFT_FOOT("left_foot.jvg",1),
-  RIGHT_FOOT("right_foot.jvg",1),
+  LEFT_HAND("left_hand.jvg",2),
+  LEFT_FOOT("left_foot.jvg",2),
+  RIGHT_FOOT("right_foot.jvg",2),
   BODY("body.jvg",1),
   LEFT_THIGH("left_thigh",1),
   RIGHT_THIGH("right_thigh",1),
