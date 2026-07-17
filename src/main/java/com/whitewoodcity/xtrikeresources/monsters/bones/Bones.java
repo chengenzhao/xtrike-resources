c9d75b7cd7fd71ebc373c82bbf6037eab4cd7b71
chengenzhao <chengen.zhao@mail.mcgill.ca>
2026-07-17 09:58:51 +0800

    attack of bones

--- src/main/java/com/whitewoodcity/xtrikeresources/monsters/bones/Bones.java
+++ src/main/java/com/whitewoodcity/xtrikeresources/monsters/bones/Bones.java
@@ -10,5 +10,5 @@ import static com.whitewoodcity.xtrikeresources.monsters.bones.Components.*;
 
 public class Bones {
   public static final List<JVGComponent> JVG_COMPONENTS = List.of(RIGHT_ARM,BLADE,RIGHT_THIGH, RIGHT_FOOT,LEFT_THIGH, BODY, LEFT_FOOT,  LEFT_SHOULDER, LEFT_HAND, LEFT_ARM, HEAD);
-  public static final List<JsonFile> ACTIONS              = List.of(IDLE,WALK);
+  public static final List<JsonFile> ACTIONS              = List.of(IDLE,WALK, ATTACK);
 }
