95f207de3c909304683533af7359f0c66c99e8ce
chengenzhao <chengen.zhao@mail.mcgill.ca>
2026-08-16 11:02:26 +0800

    releasing 1.5.2 refactor of orcus

--- src/main/java/com/whitewoodcity/xtrikeresources/monsters/orcus/Orcus.java
+++ src/main/java/com/whitewoodcity/xtrikeresources/monsters/orcus/Orcus.java
@@ -10,6 +10,6 @@ import static com.whitewoodcity.xtrikeresources.monsters.orcus.Components.*;
 
 
 public class Orcus {
-  public static final List<JVGComponent> JVG_COMPONENTS = List.of(RIGHT_THIGH, RIGHT_LEG, RIGHT_SHOE, RIGHT_HAND, LEFT_THIGH, LEFT_LEG, LEFT_SHOE, RIGHT_ARM, BODY, LEFT_ARM, HEAD, POLE_AXE, LEFT_PALM,LEFT_HAND,RIGHT_PALM);
+  public static final List<JVGComponent> JVG_COMPONENTS = List.of(RIGHT_THIGH, RIGHT_LEG, RIGHT_SHOE, RIGHT_HAND, RIGHT_ARM, BODY,LEFT_THIGH, LEFT_LEG, LEFT_SHOE,  LEFT_ARM, HEAD, POLE_AXE, LEFT_PALM,LEFT_HAND,RIGHT_PALM);
   public static final List<JsonFile> ACTIONS            = List.of(IDLE,WALK, ATTACK);
 }
