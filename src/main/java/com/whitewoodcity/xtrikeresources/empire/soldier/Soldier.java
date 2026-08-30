370ae0ea665222cc76a896a677cbed81cec9eb78
chengenzhao001 <chengen.zhao@mail.mcgill.ca>
2026-08-30 16:28:13 +0800

    ajdust the order of body components

--- src/main/java/com/whitewoodcity/xtrikeresources/empire/soldier/Soldier.java
+++ src/main/java/com/whitewoodcity/xtrikeresources/empire/soldier/Soldier.java
@@ -8,6 +8,6 @@ import static com.whitewoodcity.xtrikeresources.empire.soldier.Components.*;
 import static com.whitewoodcity.xtrikeresources.empire.soldier.Action.*;
 
 public class Soldier {
-  public static final List<Components> COMPONENTS       = List.of(LEFT_CALF,LEFT_SHOE,RIGHT_SHOE,RIGHT_CALF,RIGHT_THIGH,RIGHT_ARM,BODY,LEFT_THIGH,GUN,RIGHT_HAND,LEFT_ARM,HEAD,LEFT_SHOULDER);
+  public static final List<Components> COMPONENTS       = List.of(RIGHT_SHOE,RIGHT_CALF,RIGHT_THIGH,RIGHT_ARM,BODY,LEFT_CALF,LEFT_SHOE,LEFT_THIGH,GUN,RIGHT_HAND,LEFT_ARM,HEAD,LEFT_SHOULDER);
   public static final List<JsonFile> ACTIONS            = List.of(IDLE);
 }
